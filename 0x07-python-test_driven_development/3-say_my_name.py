60f1992295978118fcc03db7254df103434d0c4a
cynraw <cynthiaronoh@gmail.com>
2024-02-01 04:06:40 -0800

    2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""3-say_my_name, created for task 2 of python test driven development.
+"""
+
 def say_my_name(first_name, last_name=""):
+    """say_my_name, a function that prints the name of the person passed.
+    Args:
+    first_name- string 
+    last_name- string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
